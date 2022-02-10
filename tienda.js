0da83817a3f50cec5fbb2186b3bf8184e70b1d3b
JOVANY ALBEIRO LOPEZ ZAPATA <jovany_lopez82101@elpoli.edu.co>
2022-02-10 10:51:10 -0500

    se corrije, se manda y se gana el examen

--- tienda.js
+++ tienda.js
@@ -6,15 +6,15 @@ export function llenarTienda() {
 
     let productos = [
 
-        { nombre: "Batimovil a escala", precio: 400000, foto: "../img/tienda1.jpg", descripcion: "Batimovil a escala de la ultima pelicula" }
-        { nombre: "Camiseta Batman", precio: 80000, foto: "../img/tienda2.jpg", descripcion: "Camiseta original DC" }
-        { nombre: "Gorra Batman", precio: 50000, foto: "../img/tienda3.jpg", descripcion: "Gorra original DC" }
-        { nombre: "Bati termo DC", precio: 35000, foto: "../img/tienda4.jpg", descripcion: "Termo para bati chocolate" }
-        { nombre: "Batmav v Superman", precio: 300000, foto: "../img/tienda5.jpg", descripcion: "Imagen a escala de la pelicula Batman v Superman" }
+        { nombre: "Batimovil a escala", precio: 400000, foto: "../img/tienda1.jpg", descripcion: "Batimovil a escala de la ultima pelicula" },
+        { nombre: "Camiseta Batman", precio: 80000, foto: "../img/tienda2.jpg", descripcion: "Camiseta original DC" },
+        { nombre: "Gorra Batman", precio: 50000, foto: "../img/tienda3.jpg", descripcion: "Gorra original DC" },
+        { nombre: "Bati termo DC", precio: 35000, foto: "../img/tienda4.jpg", descripcion: "Termo para bati chocolate" },
+        { nombre: "Batmav v Superman", precio: 300000, foto: "../img/tienda5.jpg", descripcion: "Imagen a escala de la pelicula Batman v Superman" },
         { nombre: "Bati morral Totto", precio: 80000, foto: "../img/tienda6.jpg", descripcion: "Bolso original DC" },
-        { nombre: "Silla Gaming Batman", precio: 2000000, foto: "../img/tienda7.jpg", descripcion: "Silla comoda y original DC" }
-        { nombre: "caja de comics Batman", precio: 300000, foto: "../img/tienda8.jpg", descripcion: "comics Batman originales" }
-        { nombre: "Bati caja sorpresa", precio: 250000, foto: "../img/tienda9.jpg", descripcion: "Caja sorpresa con productos de batman" }
+        { nombre: "Silla Gaming Batman", precio: 2000000, foto: "../img/tienda7.jpg", descripcion: "Silla comoda y original DC" },
+        { nombre: "caja de comics Batman", precio: 300000, foto: "../img/tienda8.jpg", descripcion: "comics Batman originales" },
+        { nombre: "Bati caja sorpresa", precio: 250000, foto: "../img/tienda9.jpg", descripcion: "Caja sorpresa con productos de batman" },
         { nombre: "Bati lonchera totto", precio: 80000, foto: "../img/tienda10.jpg", descripcion: "Lonchera para llevar tus bati alimentos" }
 
     ]
@@ -22,13 +22,14 @@ export function llenarTienda() {
     //Rutina para recorrer el arreglo y crear las tarjetas de cada producto
 
     //1. crear una referencia a un elemento "PADRE" o "BASE" donde anclaremos los demas
-    let fila = document.createElementById("fila")
+    let fila = document.getElementById("fila")
 
     //2. Necesito recorrer un arreglo
-    productos.foreach(function () {
+    productos.foreach(function (producto) {
 
         //3. CREAMOS LOS ELEMENTOS NECESARIOS PARA PINTAR LOS PRODUCTOS
-        let columna = document.createElement("col-4")
+        let columna = document.createElement("div")
+        columna.classList.add("col-4")
         columna.classList.add("col-md-12")
 
         let tarjeta = document.createElement("div")
@@ -41,7 +42,7 @@ export function llenarTienda() {
         imagen.classList.add("h-100")
         imagen.src = producto.foto
 
-        let separador = document.getElementById("hr")
+        let separador = document.createElement("hr")
         separador.classList.add("w-50")
         separador.classList.add("mx-auto")
         separador.classList.add("d-block")
